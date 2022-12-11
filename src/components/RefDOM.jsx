ac3195c1322dbbf4293c06a353a232ed28b03e17
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-11 12:45:33 +0900

    useRef로 DOM 다루기

---
+++ src/components/RefDOM.jsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+
+export default function RefDOM() {
+	const orangeEl = useRef();
+	const skyblueEl = useRef();
+	const inputEl = useRef();
+
+	const adjustCSS = () => {
+		orangeEl.current.style.backgroundColor = "orange";
+		skyblueEl.current.style.backgroundColor = "skyblue";
+	};
+
+	// **.current 까지 접근해야 돔요소에 접근 가능
+	const clearInput = () => {
+		inputEl.current.value = "";
+	};
+
+	return (
+		<div>
+			<h1 ref={orangeEl}>ORANGE</h1>
+			<h1 ref={skyblueEl}>SKYBLUE</h1>
+			<input ref={inputEl} />
+			<br />
+			<button onClick={adjustCSS}>CSS 적용</button>
+			<button onClick={clearInput}>input 초기화</button>
+		</div>
+	);
+}
---
+++ src/components/TestRef.jsx
@@ -0,0 +1,22 @@
+import { useState, useRef } from "react";
+
+export default function TestRef() {
+	let [text, setText] = useState("안녕하세여!");
+
+	const inputValue = useRef();
+	const onChangeText = (e) => {
+		const inputText = inputValue.current.value; // 인풋에 변화되는 값 (target -> DOM 요소)
+		setText(inputText);
+	};
+
+	return (
+		<div>
+			<h1>{text}</h1> {/* 초기 스테이트 값 지정 */}
+			<input
+				ref={inputValue}
+				onChange={(e) => {
+					onChangeText();
+				}}></input>
+		</div>
+	);
+}
