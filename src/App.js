bd5c2da310077d07f825e2d0672539807be307a7
Kim Jiae <95462028+gimziae@users.noreply.github.com>
2022-12-12 21:31:50 +0900

    styled components 실습

--- src/App.js
+++ src/App.js
@@ -30,14 +30,59 @@ import UsingUseMemo from "./components/UsingUseMemo";
 import TestInlineCSS from "./components/TestInlineCSS";
 import TestCssFile from "./components/TestCssFile";
 import TestStyled from "./components/TestStyled";
+import logo from "./img/cake.jpeg";
+import styled, { keyframes } from "styled-components";
+
+const AppLogoSpin = keyframes`
+from {
+	transform: rotate(0deg);
+}
+to {
+	transform: rotate(360deg);
+}
+`;
+
+const RootDiv = styled.div`
+	text-align: center;
+`;
+const AppHeader = styled.header`
+	background-color: #282c34;
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+`;
+const AppLogo = styled.img`
+	height: 40vmin;
+	pointer-events: none;
+	border-radius: 50%;
+	animation: ${AppLogoSpin} infinite 20s linear;
+`;
+const MyA = styled.a`
+	color: #61dafb;
+`;
 
 function App() {
 	return (
-		<div className="App">
-			<TestInlineCSS />
-			<TestCssFile />
-			<TestStyled />
-		</div>
+		<RootDiv>
+			<AppHeader>
+				<AppLogo src={logo} alt="app" />
+				<p>
+					Styled 적용을 위해 변경 된 <br />
+					App.js Edit
+					<code> src/App.js</code> and save to reload.{" "}
+				</p>
+				<MyA
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer">
+					Learn React
+				</MyA>
+			</AppHeader>
+		</RootDiv>
 	);
 }
 
